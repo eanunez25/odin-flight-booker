35c62de4844ef431e312bad5ef6d2e0a46a64363
Elvin Nunez <eanunez25@gmail.com>
2020-08-27 13:50:38 -0500

    creat and seed Airport model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+code_list = [
+  "ORD", "LAX", "PHX", "SFO", "NYC", "ATL", "JFK", "SEA", "PHX", "LGA"
+]
+
+code_list.each do |code|
+  Airport.create(code: code)
+end
