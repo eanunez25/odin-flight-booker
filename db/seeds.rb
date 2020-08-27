6f5f5c8bc20625080b583aa35b1cc151ee8cb19b
Elvin Nunez <eanunez25@gmail.com>
2020-08-27 16:59:05 -0500

    seed db with flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,30 @@ code_list = [
 code_list.each do |code|
   Airport.create(code: code)
 end
+
+flight_list = [
+  [1, 2],
+  [1, 6],
+  [2, 5], 
+  [2, 10], 
+  [3, 4], 
+  [3, 8], 
+  [4, 2], 
+  [4, 6],
+  [5, 8], 
+  [5, 3],
+  [6, 9],
+  [6, 1], 
+  [7, 4], 
+  [7, 9], 
+  [8, 5],
+  [8, 10],
+  [9, 2], 
+  [9, 7],
+  [10, 1], 
+  [10, 4]
+]
+
+flight_list.each do |from, to|
+  Flight.create(start_id: from, end_id: to)
+end
