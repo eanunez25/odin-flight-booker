b379194d5d0442caf33f8f98cba18c0d1bfe064f
Elvin Nunez <eanunez25@gmail.com>
2020-08-29 21:12:30 -0500

    create bookings controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,14 @@
 class FlightsController < ApplicationController
   def index
     @dates = Flight.get_dates
-
-    date = Date.parse(params[:date])
-    begin_day = date.beginning_of_day
-    end_day = date.end_of_day
-    @flights = Flight.where("start_id = ? AND end_id = ? AND takeoff >= ? AND takeoff <= ?", params[:from_airport], params[:to_airport], begin_day, end_day)
+    @flights = []
+    if params[:date]
+      date = Date.parse(params[:date])
+      begin_day = date.beginning_of_day
+      end_day = date.end_of_day
+      @flights = Flight.where("start_id = ? AND end_id = ? AND takeoff >= ? AND takeoff <= ?", params[:from_airport], params[:to_airport], begin_day, end_day)
+    end
+    
   end
 
   private
